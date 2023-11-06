8f500c77d30b0ef33171ccbd75a8d6c8c68dbd52
Lhhiep-maxcode <122993600+Lhhiep-maxcode@users.noreply.github.com>
2023-11-06 09:33:24 +0700

    Update HelloApplication.java

--- gittest/src/main/java/com/example/gittest/HelloApplication.java
+++ gittest/src/main/java/com/example/gittest/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 
-        stage.setTitle("Broooo Codeeee ok nheeeeee!");
+        stage.setTitle("tao khong biet may la ai!");
 
         stage.setScene(scene);
         stage.show();
